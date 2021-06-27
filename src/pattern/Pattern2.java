6970288f6ff61e497b7d60646444da94eadf1934
Prajjwal <prajjwal199275@gmail.com>
2021-06-27 22:05:30 +0530

    Create a Pattern2.java class

---
+++ src/pattern/Pattern2.java
@@ -0,0 +1,11 @@
+/* Created by IntelliJ IDEA.
+
+    Author: Prajjwal Pachauri
+    Date: 27-06-2021
+    Time: 10:04 PM
+    File: Pattern2.java 
+*/
+package pattern;
+
+public class Pattern2 {
+}
